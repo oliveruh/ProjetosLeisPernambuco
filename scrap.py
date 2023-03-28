2ef2cc11f2cdadbb315d9df0da39352ff2533a7c
oliverbot <herlisson.ruh@gmail.com>
2023-03-28 12:52:02 -0300

    fix: refactor scrapper main

--- scrap.py
+++ scrap.py
@@ -1,10 +1,15 @@
+import logging
+import os
+
 from app import create_app
+
 from scrapper.proposal_list import scrap_current_proposals
 from scrapper.proposal import scrap_new_law_proposal
-from app.models import ProjetoDeLei
-from app.models import Proposicao
-from app.models import ProjetoDeLeiResumo
-import os
+
+from app.models import ProjetoDeLei, ProjetoDeLeiResumo, Proposicao
+
+log = logging.getLogger(__name__)
+
 
 create_app().app_context().push()
 
@@ -14,43 +19,55 @@ def success_scrapping(value):
     with open(env_file, "a") as myfile:
         myfile.write(f"SUCCESS_SCRAPPING={value}")
 
-proposals = scrap_current_proposals()
-last_proposal_date = ProjetoDeLei.get_last_dataPublicacao()
+def find_new_proposals(proposals, last_proposal_date):
+    log.info('Searching for new proposals...')
 
-print('Searching for new proposals...')
+    new_proposals = [proposal for proposal in proposals if proposal['Data da publicação'] > last_proposal_date]
 
-new_proposals = [proposal for proposal in proposals if proposal['Data da publicação'] > last_proposal_date]
+    qntd_proposals = len(new_proposals)
+    new_proposals_msg = 'No new proposals found!'
+    success_status = 'false'
 
-qntd_proposals = len(new_proposals)
-new_proposals_msg = 'No new proposals found!'
-success_status = 'false'
+    if qntd_proposals > 0:
+        success_status = 'true'
+        new_proposals_msg = f'New proposal{"s" if qntd_proposals > 1 else ""} found!'
 
-if qntd_proposals > 0:
-    success_status = 'true'
-    new_proposals_msg = f'New proposal{"s" if qntd_proposals > 1 else ""} found!'
+        if qntd_proposals > 50:
+            new_proposals_msg += '\nMore than 50 proposals found!\nGoing to third page...'
+            new_proposals.extend(scrap_current_proposals(3))
+        elif qntd_proposals > 25:
+            new_proposals_msg += '\nMore than 25 proposals found!\nGoing to second page...'
+            new_proposals.extend(scrap_current_proposals(2))
 
-    if qntd_proposals > 50:
-        new_proposals_msg += '\nMore than 50 proposals found!\nGoing to third page...'
-        new_proposals.extend(scrap_current_proposals(3))
-    elif qntd_proposals > 25:
-        new_proposals_msg += '\nMore than 25 proposals found!\nGoing to second page...'
-        new_proposals.extend(scrap_current_proposals(2))
+    success_scrapping(success_status)
+    log.info(new_proposals_msg)
 
-success_scrapping(success_status)
-print(new_proposals_msg)
+    return new_proposals
 
-for i in range(len(new_proposals)):
-    try:
-        resumo = scrap_new_law_proposal(new_proposals[i], i)
-    except Exception as e:
-        print(e)
-        print('DEU ERRO!')
-        continue
-    ProjetoDeLei.add(new_proposals[i]['Id'], new_proposals[i]['Proposição'], new_proposals[i]['Data da publicação'], new_proposals[i]['Link'])
-    Proposicao.add(new_proposals[i]['Autor'], new_proposals[i]['Id'])
-    ProjetoDeLeiResumo.add(resumo['RESUMO'], new_proposals[i]['Id'], resumo['TITULO'])
+def proccess_new_proposals(new_proposals):
+    log.info('Proccessing new proposals...')
 
-print('Finishing adding proposals to database!')
+    for index, proposal in enumerate(new_proposals):
+        try:
+            resumo = scrap_new_law_proposal(proposal, index)
+        except Exception as e:
+            log.error(e)
+            log.error('Error occurred!')
+            continue
 
+        ProjetoDeLei.add(proposal['Id'], proposal['Proposição'], proposal['Data da publicação'], proposal['Link'])
+        Proposicao.add(proposal['Autor'], proposal['Id'])
+        ProjetoDeLeiResumo.add(resumo['RESUMO'], proposal['Id'], resumo['TITULO'])
 
+    log.info('Finished adding proposals to database!')
 
+def main():
+    proposals = scrap_current_proposals()
+    last_proposal_date = ProjetoDeLei.get_last_dataPublicacao()
+
+    new_proposals = find_new_proposals(proposals, last_proposal_date)
+
+    proccess_new_proposals(new_proposals)
+
+if __name__ == '__main__':
+    main()
