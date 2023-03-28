eb78cbf2970999c6f231e21f8608c1b0bfb6411f
oliverbot <herlisson.ruh@gmail.com>
2023-03-27 22:20:46 -0300

    calling scripts on lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,11 @@
 import json
+import subprocess
+
 
 def lambda_handler(event, context):
-    # TODO implement
+    subprocess.run(["python", "scrap.py"])
+    subprocess.run(["python", "freeze.py"])
+    subprocess.run(["python", "s3_upload.py"])
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
